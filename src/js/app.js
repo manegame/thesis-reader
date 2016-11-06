25848f456069986a0660868f4c7673d1f83a9c62
manegame <manusnijhoff@gmail.com>
2016-11-06 23:36:55 +0100

    Inserted template

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+/*jslint browser: true, devel: true, node: true, nomen: true, plusplus: true*/
+/*global $, jQuery*/
+
+(function () {
+
+  "use strict";
+  
+  // Require jQuery
+  global.$                = require("jquery");
+    
+  var shared              = require("./shared"),
+      debounce            = require('debounce');
+      
+  //  require("swiper");
+  //  require("fullpage.js");
+  
+  // Modernizr tests
+  // require('browsernizr/test/webgl');
+  // require('browsernizr/test/workers/webworkers');
+  // require('browsernizr/test/webrtc/peerconnection');
+  // require('browsernizr/test/storage/localstorage');
+  // require('browsernizr/test/audio/webaudio');
+  // require('browsernizr/test/websockets');
+  // var Modernizr = require('browsernizr');
+  
+  $(function () {
+    
+    console.log("READY");
+    
+    //    var swiper = new Swiper('.swiper-container', {
+    //      speed: 1000
+    //      autoplay: 40
+    //    });
+
+    //    $('#fullpage').fullpage();  
+
+  });
+
+}());
